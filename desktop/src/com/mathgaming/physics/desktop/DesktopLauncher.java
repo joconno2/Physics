250a75d4226e8b52df39641cd7b69dd7d7b9b9d3
Jim O'Connor <joconno2@conncoll.edu>
2015-04-23 18:02:46 -0400

    A new LibGDX physics test

---
+++ desktop/src/com/mathgaming/physics/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.mathgaming.physics.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mathgaming.physics.Physics;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Physics(), config);
+	}
+}
---
+++ html/src/com/mathgaming/physics/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.mathgaming.physics.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.mathgaming.physics.Physics;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new Physics();
+        }
+}
